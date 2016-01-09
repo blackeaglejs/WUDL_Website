ab60571bc7a62c680b34684dbf0ceb3cbf8954d0
Austin Bae <1551780@gmail.com>
2016-01-09 17:06:05 -0500

    added admin features and added global helpers

--- client/helpers.js
+++ client/helpers.js
@@ -1,39 +1,28 @@
 Template.teachers.helpers({
 	teachers: function(){
 		return Teachers.findOne({});
-	},
-	optionsHelper: function () {
-		return {
-			wysiwyg: true,
-			eventType: "dblclick",
-			textarea: true,
-			showToolbar: true
-		};
 	}
 });
 Template.programming.helpers({
 	programming: function(){
 		return Programming.findOne({});
-	},
-	optionsHelper: function () {
-		return {
-			wysiwyg: true,
-			eventType: "dblclick",
-			textarea: true,
-			showToolbar: true
-		};
 	}
 });
 Template.about.helpers({
 	about: function(){
 		return About.findOne({});
-	},
-	optionsHelper: function () {
-		return {
+	}
+	
+});
+
+Template.registerHelper('optionsHelper', function(){
+	return {
 			wysiwyg: true,
 			eventType: "dblclick",
 			textarea: true,
 			showToolbar: true
 		};
-	}
+});
+Template.registerHelper('isAdmin', function(){
+	return Meteor.userId()===Meteor.users.findOne({username: "admin"})["_id"];
 });
