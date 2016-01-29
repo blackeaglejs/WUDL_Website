bf3a1310a6febbaf3f64c853e7a75220163601f1
Zoheb <Zoheb2@zohebneomputer2.home>
2016-01-28 20:41:50 -0500

    Adding route file to fix merge conflict

---
+++ client/route.js
@@ -0,0 +1,36 @@
+Router.configure({
+	layoutTemplate: 'layout',
+	yieldTemplates: {
+		'home': {to: 'home'},
+		'about': {to: 'about'},
+		'programming': {to: 'programming'},
+		'contact': {to: 'contact'},
+		'impact': {to: 'impact'},
+		'involve': {to: 'involve'},
+		'students': {to: 'students'},
+		'teachers': {to: 'teachers'},
+		'meet-the-team': {to: 'meet-the-team'},
+		'schedule': {to: 'schedule'},
+		'tournaments': {to: 'tournaments'},
+		'udlmodel': {to: 'udlmodel'},
+		'camp': {to: 'camp'}
+		'ProfessionalDevelopment': {to: 'ProfessionalDevelopment'}
+	}
+});
+
+Router.map(function(){
+	this.route('Home', {
+		path: '/',
+		template: 'layout',
+		data: function(){
+			Session.set('url', 'home');
+		}
+	});
+	this.route('Pages', {
+		path: '/:name',
+		template: 'layout',
+		data: function(){
+			Session.set('url', this.params.name);
+		}
+	});
+});
